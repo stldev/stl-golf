c2d15797380af7af734c28541d838c16d4584f54
rbb <rick1stl@gmail.com>
2022-07-28 22:37:02 -0500

    correct values

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import { FirebaseOptions, initializeApp } from 'firebase/app';
 import './router';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: 'AIzaSyBkvTfAcVuKxT_uLc6wj66I4uWppK-hJYE',
-  authDomain: 'rbb-cleaning.firebaseapp.com',
-  databaseURL: 'https://rbb-cleaning-default-rtdb.firebaseio.com',
-  projectId: 'rbb-cleaning',
-  storageBucket: 'rbb-cleaning.appspot.com',
-  messagingSenderId: '751365085821',
-  appId: '1:751365085821:web:88bf4fd16ae090b1036f14',
+  apiKey: 'AIzaSyBDVyGbyl7fHEKmPOkVGR8mjgLgqigj2zY',
+  authDomain: 'woodchopper-golf.firebaseapp.com',
+  databaseURL: 'https://woodchopper-golf-default-rtdb.firebaseio.com',
+  projectId: 'woodchopper-golf',
+  storageBucket: 'woodchopper-golf.appspot.com',
+  messagingSenderId: '1038321925318',
+  appId: '1:1038321925318:web:5d1637514a022894cbebc7',
 };
 
 initializeApp(firebaseConfig);
