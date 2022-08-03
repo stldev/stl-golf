eceda1a6a5ae4355069bc57950a2270cf7d84497
rbb <rick1stl@gmail.com>
2022-08-03 01:41:32 -0500

    move firebase service

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-import { FirebaseOptions, initializeApp } from 'firebase/app';
+import './services/firebase';
+import './store/index';
 import './router';
-
-const firebaseConfig: FirebaseOptions = {
-  apiKey: 'AIzaSyBDVyGbyl7fHEKmPOkVGR8mjgLgqigj2zY',
-  authDomain: 'woodchopper-golf.firebaseapp.com',
-  databaseURL: 'https://woodchopper-golf-default-rtdb.firebaseio.com',
-  projectId: 'woodchopper-golf',
-  storageBucket: 'woodchopper-golf.appspot.com',
-  messagingSenderId: '1038321925318',
-  appId: '1:1038321925318:web:5d1637514a022894cbebc7',
-};
-
-initializeApp(firebaseConfig);
