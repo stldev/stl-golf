d9eaa0f8916e926fc5f79447cb4d1ef3eaca6da1
rbb <rick1stl@gmail.com>
2022-08-03 01:42:05 -0500

    add models file

---
+++ src/models/index.ts
@@ -0,0 +1,15 @@
+// export interface ICamera {
+//   locationId: string;
+//   name: string;
+//   ip: string;
+//   imgBase64: string;
+// }
+
+// export interface ICameraVars {
+//   name: string;
+//   ip: string;
+// }
+
+// export interface IGlobalThis extends Window {
+//   swRegistration?: ServiceWorkerRegistration;
+// }
