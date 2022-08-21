2cc004e384d755ebfecf7a82223e1950599d9bb4
rbb <rick1stl@gmail.com>
2022-08-21 11:21:50 -0500

    test

--- src/views/Settings.ts
+++ src/views/Settings.ts
@@ -107,7 +107,7 @@ export class Settings extends LitElement {
                 APPLY UPDATE
               </button>`
             : ''}
-          <h2>App Settings!</h2>
+          <h2>App Settings</h2>
         </header>
         <section>
           ${this.newUpdateAvailable
