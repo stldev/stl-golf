9ebf51e4c961e1e510489039e4b5931011ee5d2a
stldev <75532143+stldev@users.noreply.github.com>
2022-08-23 22:33:35 -0500

    Update auth-guard.ts

--- src/services/auth-guard.ts
+++ src/services/auth-guard.ts
@@ -2,7 +2,7 @@ import { storeSvc } from '../store/data';
 
 export async function authGuard(context: any, commands: any) {
   const team = localStorage.getItem('woodchopper-team');
-  console.log('authGuard-team', team);
+  console.log('authGuard-team:', team);
   if (team) {
     storeSvc.getCourse();
     storeSvc.getSchedule(team);
